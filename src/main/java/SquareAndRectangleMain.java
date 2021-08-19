129e9d213e7721ac44988d53f0cc85e813a020b1
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-19 09:09:06 +0530

    Requirement 1 : Calculated Area of Square .

--- src/main/java/SquareAndRectangleMain.java
+++ src/main/java/SquareAndRectangleMain.java
@@ -1,2 +1,10 @@
 public class SquareAndRectangleMain {
+    double side;
+    public SquareAndRectangleMain(double side)
+    {
+        this.side = side;
+    }
+    public double areaOfSquare() {
+        return side * side;
+    }
 }
--- src/test/java/SquareAndRectangleTest.java
+++ src/test/java/SquareAndRectangleTest.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SquareAndRectangleTest {
+    @Nested
+    public class squareOperations {
+        @DisplayName("Area of Square with Positive Sides")
+        @Test
+        void toImplementAreaOfSquareWithPositiveSides() {
+            SquareAndRectangleMain object = new SquareAndRectangleMain(5);
+            assertEquals(25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Positive Sides")
+        @Test
+        void toImplementAreaOfSquareWithNegativeSides() {
+            SquareAndRectangleMain object = new SquareAndRectangleMain(-5);
+            assertEquals(25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Positive Decimal side")
+        @Test
+        void toImplementAreaOfSquareWithPositiveDecimalSides() {
+            SquareAndRectangleMain object = new SquareAndRectangleMain(5.5);
+            assertEquals(30.25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Negative Decimal side")
+        @Test
+        void toImplementAreaOfSquareWithNegativeDecimalSides() {
+            SquareAndRectangleMain object = new SquareAndRectangleMain(-5.5);
+            assertEquals(30.25, object.areaOfSquare());
+        }
+    }
 }
